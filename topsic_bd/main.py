68eda858103fccb2454720ab5da33e835f9b2d49
山下卓也 <yamashitatakuya@yamashitatakuyanoMacBook-Pro.local>
2022-05-09 11:26:07 +0900

    互いに異なる(修正）

--- topsic_bd/main.py
+++ topsic_bd/main.py
@@ -1,11 +1,13 @@
-import collections
-N = int(input())
-A = list(map(int ,input().split()))
-C = collections.Counter(A)
+n = int(input())
+a = list(map(int ,input().split()))
+A = sorted(a)
+ans = 0
 
+for i in range(n):
+    if A[i] == A[i-1]:
+        ans += 1
 
-#グループ集計を左から降順に行い、一番左に数字が１以上（複数あるか）でYESかNOを定義
-if C.most_common()[0][0] > 1:
-	print("NO")
-elif C.most_common()[0][0] <= 1:
+if ans >= 1:
+    print("NO")
+elif ans == 0:
     print("YES")
